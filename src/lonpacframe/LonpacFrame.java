6da1ef3ded7f8688493645276b34342426858fe4
tungnx <tungnx@vnpay.vn>
2023-07-07 17:02:31 +0700

    add

---
+++ src/lonpacframe/LonpacFrame.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lonpacframe;
+
+/**
+ *
+ * @author Sara
+ */
+public class LonpacFrame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        userregistration userregistration = new userregistration();
+//        userregistration.main();
+//        userregistration.st
+        userregistration.main(args);
+    }
+    
+}
